b4c44d227158ee49701b427b1e94b9b955cd21f2
Kristoffer Alfheim <kristoffer.alfh@gmail.com>
2017-10-17 10:07:03 +0200

    wtf tabs

--- js/index.js
+++ js/index.js
@@ -1,82 +1,82 @@
 function cleanseHeadlines(extract) {
-	extract
-		.querySelectorAll([
-			'.article-content > *:not(.df-img-container)',
-			'.df-article-content > *:not(.df-img-container)',
-		].join(','))
-		.forEach(node => node.remove());
+  extract
+    .querySelectorAll([
+      '.article-content > *:not(.df-img-container)',
+      '.df-article-content > *:not(.df-img-container)',
+    ].join(','))
+    .forEach(node => node.remove());
 }
 
 function findArticleUrl(extract) {
-	const anchor = extract.querySelector('a');
+  const anchor = extract.querySelector('a');
 
-	if (! anchor || ! anchor.href) {
-		return;
-	}
+  if (! anchor || ! anchor.href) {
+    return;
+  }
 
-	if (anchor.host !== 'www.vg.no') {
-		return;
-	}
+  if (anchor.host !== 'www.vg.no') {
+    return;
+  }
 
-	return anchor.href.replace('http://', 'https://');
+  return anchor.href.replace('http://', 'https://');
 }
 
 async function fetchArticleMeta(url) {
-	const response = await fetch(url);
+  const response = await fetch(url);
 
-	const value = await response.text();
+  const value = await response.text();
 
-	return {
-		title: value.match(/<meta name="twitter:title" content="([^"]+)">/)[1],
-		description: value.match(/<meta name="twitter:description" content="([^"]+)">/)[1],
-	};
+  return {
+    title: value.match(/<meta name="twitter:title" content="([^"]+)">/)[1],
+    description: value.match(/<meta name="twitter:description" content="([^"]+)">/)[1],
+  };
 }
 
 async function unwrapClickbait(extract) {
-	const articleUrl = findArticleUrl(extract);
+  const articleUrl = findArticleUrl(extract);
 
-	if (! articleUrl) {
-		return;
-	}
+  if (! articleUrl) {
+    return;
+  }
 
-	const { title, description } = await fetchArticleMeta(articleUrl);
+  const { title, description } = await fetchArticleMeta(articleUrl);
 
-	const headlines = document.createElement('div');
+  const headlines = document.createElement('div');
 
-	headlines.innerHTML = `
-		<h3 class="vg-fs38" style="margin-bottom: .5rem; line-height: .9;">
-			<span class="vg-fs38"><a href="${articleUrl}">${title}</a></span>
-		</h3>
-		<h4 class="vg-fs20">
-			<span class="vg-fs20"><a href="${articleUrl}" style="color: #444;">${description}</a></span>
-		</h4>
-	`;
+  headlines.innerHTML = `
+    <h3 class="vg-fs38" style="margin-bottom: .5rem; line-height: .9;">
+      <span class="vg-fs38"><a href="${articleUrl}">${title}</a></span>
+    </h3>
+    <h4 class="vg-fs20">
+      <span class="vg-fs20"><a href="${articleUrl}" style="color: #444;">${description}</a></span>
+    </h4>
+  `;
 
-	cleanseHeadlines(extract);
+  cleanseHeadlines(extract);
 
-	const articleContent = extract.querySelector('.article-content, .df-article-content');
+  const articleContent = extract.querySelector('.article-content, .df-article-content');
 
-	articleContent.appendChild(headlines);
+  articleContent.appendChild(headlines);
 }
 
 function initbaby() {
-	const observer = new IntersectionObserver((entries, observer) => {
-		entries
-			.filter(entry => entry.isIntersecting)
-			.forEach(entry => {
-				observer.unobserve(entry.target);
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries
+      .filter(entry => entry.isIntersecting)
+      .forEach(entry => {
+        observer.unobserve(entry.target);
 
-				unwrapClickbait(entry.target);
-			});
-	});
+        unwrapClickbait(entry.target);
+      });
+  });
 
-	const extracts = document.querySelectorAll('.article-extract, .df-article');
+  const extracts = document.querySelectorAll('.article-extract, .df-article');
 
-	extracts.forEach(extract => observer.observe(extract));
+  extracts.forEach(extract => observer.observe(extract));
 }
 
 if (document.readyState === 'complete') {
-	initbaby();
+  initbaby();
 } else {
-	document.addEventListener('load', initbaby());
+  document.addEventListener('load', initbaby());
 }
