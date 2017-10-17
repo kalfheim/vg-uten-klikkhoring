57bc99b6df2af585560f7fac908d97aaf3f9ded6
Kristoffer Alfheim <kristoffer.alfh@gmail.com>
2017-10-17 22:45:43 +0200

    v2 holy shit this is too much

--- js/index.js
+++ js/index.js
@@ -1,54 +1,117 @@
+// Totally fucked hosts. Just ignore them...
+const ignoredHosts = [
+  'vglive.no',
+  'direkte.vg.no',
+];
+
+// These guys don't support security.
+const whoGivesAFuckAboutHttps = [
+  // 'www.minmote.no',
+  // 'pluss.vg.no',
+];
+
+function superFetch(url) {
+  if (url.indexOf('https://www.vg.no/') === 0) {
+    return fetch(url);
+  }
+
+  // let's hope this thing is up most of the time
+  return fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+}
+
+async function fetchMetaFromAnything(url) {
+  const html = await (
+    await superFetch(url)
+  ).text();
+
+  return {
+    title: html.match(/<meta (?:name|property)="(?:twitter|og):title" content="([^"]+)"/)[1],
+    description: html.match(/<meta (?:name|property)="(?:twitter|og):description" content="([^"]+)"/)[1],
+  };
+}
+
+async function fetchMetaFromVGTV(url) {
+  const id = url.match(/\/video\/([0-9]+)\//)[1];
+
+  const { title, description } = await (
+    await superFetch(`https://svp.vg.no/svp/api/v1/vgtv/assets/${id}?appName=heihei`)
+  ).json();
+
+  return { title, description };
+}
+
+async function fetchMetaFromMinMote(url) {
+  const id = url.match(/\/artikkel\/([0-9]+)\//)[1];
+
+  const { title, preamble: description } = await (
+    await superFetch(`http://www.minmote.no/api/v1/articles/${id}`)
+  ).json();
+
+  return { title, description };
+}
+
+async function fetchMetaFromGodt(url) {
+  const id = url.match(/\/artikkel\/([0-9]+)\//)[1];
+
+  const { title, preamble: description } = await (
+    await superFetch(`http://www.godt.no/api/articles/${id}`)
+  ).json();
+
+  return { title, description };
+}
+
+function fetcherFetcher(host) {
+  if (ignoredHosts.indexOf(host) !== -1) {
+    return;
+  }
+
+  if (host === 'www.vgtv.no') {
+    return fetchMetaFromVGTV.bind(null);
+  }
+
+  switch (host) {
+    case 'www.vgtv.no':
+      return fetchMetaFromVGTV.bind(null);
+
+    case 'www.minmote.no':
+      return fetchMetaFromMinMote.bind(null);
+
+    case 'www.godt.no':
+      return fetchMetaFromGodt.bind(null);
+
+    default:
+      return fetchMetaFromAnything.bind(null);
+  }
+}
+
 function cleanseHeadlines(extract) {
   extract
     .querySelectorAll([
-      '.article-content > *:not(.df-img-container)',
-      '.df-article-content > *:not(.df-img-container)',
+      '.article-content > h3',
+      '.article-content > h4',
+      '.df-article-content > h3',
+      '.df-article-content > h4',
     ].join(','))
     .forEach(node => node.remove());
 }
 
-function findArticleUrl(extract) {
+async function unwrapClickbait(extract) {
   const anchor = extract.querySelector('a');
 
   if (! anchor || ! anchor.href) {
     return;
   }
 
-  if (anchor.host !== 'www.vg.no') {
-    return;
-  }
-
-  return anchor.href.replace('http://', 'https://');
-}
-
-async function fetchArticleMeta(url) {
-  const response = await fetch(url);
-
-  const value = await response.text();
-
-  return {
-    title: value.match(/<meta name="twitter:title" content="([^"]+)">/)[1],
-    description: value.match(/<meta name="twitter:description" content="([^"]+)">/)[1],
-  };
-}
-
-async function unwrapClickbait(extract) {
-  const articleUrl = findArticleUrl(extract);
-
-  if (! articleUrl) {
-    return;
-  }
-
-  const { title, description } = await fetchArticleMeta(articleUrl);
+  const { title, description } = await (fetcherFetcher(anchor.host))(anchor.href);
 
   const headlines = document.createElement('div');
 
   headlines.innerHTML = `
     <h3 class="vg-fs38" style="margin-bottom: .5rem; line-height: .9;">
-      <span class="vg-fs38"><a href="${articleUrl}">${title}</a></span>
+      <span class="vg-fs38"><a href="${anchor.href}">${title}</a></span>
     </h3>
     <h4 class="vg-fs20">
-      <span class="vg-fs20"><a href="${articleUrl}" style="color: #444;">${description}</a></span>
+      <span class="vg-fs20"><a href="${anchor.href}" style="color: #444;">${description}</a></span>
     </h4>
   `;
 
