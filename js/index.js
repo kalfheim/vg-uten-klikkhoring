790b5fb134bed1943306df5f7424e132b5509f11
Kristoffer Alfheim <kristoffer.alfh@gmail.com>
2017-10-18 15:34:30 +0200

    v3: this time with more spins

--- js/index.js
+++ js/index.js
@@ -4,6 +4,14 @@ const ignoredHosts = [
   'direkte.vg.no',
 ];
 
+function injectCss(css) {
+  const element = document.createElement('style');
+
+  element.appendChild(document.createTextNode(css));
+
+  document.head.appendChild(element);
+}
+
 function superFetch(url) {
   if (url.indexOf('https://www.vg.no/') === 0) {
     return fetch(url);
@@ -79,14 +87,18 @@ function fetcherFetcher(host) {
 }
 
 function cleanseHeadlines(extract) {
-  extract
-    .querySelectorAll([
-      '.article-content > h3',
-      '.article-content > h4',
-      '.df-article-content > h3',
-      '.df-article-content > h4',
-    ].join(','))
-    .forEach(node => node.remove());
+  const clickbaits = extract.querySelectorAll([
+    '.article-content > h3',
+    '.article-content > h4',
+    '.df-article-content > h3',
+    '.df-article-content > h4',
+  ].join(','));
+
+  const originalHeadline = Array.from(clickbaits).map(node => node.innerText).join('\n');
+
+  clickbaits.forEach(node => node.remove());
+
+  return originalHeadline;
 }
 
 async function unwrapClickbait(extract) {
@@ -98,18 +110,20 @@ async function unwrapClickbait(extract) {
 
   const { title, description } = await (fetcherFetcher(anchor.host))(anchor.href);
 
+  const originalHeadline = cleanseHeadlines(extract);
+
   const headlines = document.createElement('div');
 
   headlines.innerHTML = `
-    <h3 class="vg-fs38" style="margin-bottom: .5rem; line-height: .9;">
-      <span class="vg-fs38"><a href="${anchor.href}">${title}</a></span>
-    </h3>
-    <h4 class="vg-fs20">
-      <span class="vg-fs20"><a href="${anchor.href}" style="color: #444;">${description}</a></span>
-    </h4>
+    <div class="no-clickbait-here" data-original="${originalHeadline}">
+      <h3 class="vg-fs38">
+        <span class="vg-fs38"><a href="${anchor.href}">${title}</a></span>
+      </h3>
+      <h4 class="vg-fs20">
+        <span class="vg-fs20"><a href="${anchor.href}">${description}</a></span>
+      </h4>
+    </div>
   `;
-
-  cleanseHeadlines(extract);
 
   const articleContent = extract.querySelector('.article-content, .df-article-content');
 
@@ -117,6 +131,45 @@ async function unwrapClickbait(extract) {
 }
 
 function initbaby() {
+  injectCss(`
+    .no-clickbait-here h3 {
+      position: relative;
+      margin-bottom: .5rem;
+      line-height: .9;
+    }
+
+    .no-clickbait-here {
+      position: relative;
+    }
+
+    .no-clickbait-here::before {
+      position: absolute;
+      left: .5rem;
+      top: 0;
+      content: attr(data-original);
+      opacity: 0;
+      transition: transform ease-in-out .25s,
+                  opacity ease-in-out .15s;
+      font-size: 1.25rem;
+      font-family: Liberation Serif, Times New Roman, Times, serif;
+      font-weight: bold;
+      color: #000;
+      background-color: #fff;
+      padding: .5rem .75rem;
+      border-radius: .125rem;
+      box-shadow: 0 .125rem .25rem 0 rgba(0,0,0,.1);
+    }
+
+    .no-clickbait-here:hover::before {
+      opacity: 1;
+      transform: translateY(-135%) rotate(360deg);
+    }
+
+    .no-clickbait-here a {
+      color: #444;
+    }
+  `);
+
   const observer = new IntersectionObserver((entries, observer) => {
     entries
       .filter(entry => entry.isIntersecting)
